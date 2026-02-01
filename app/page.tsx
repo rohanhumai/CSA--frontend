90728e1bd7c44ae7fc7b45620790a90bf40adcee
Rohan Coder <sablerohan125@gmail.com>
2026-02-01 21:51:28 +0530

    initialized CTA

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import { Homepage } from "./Pages/Homepage";
+import { Cta } from "./components/Cta";
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <Homepage />
+    <Cta />
     </>
   );
 }
