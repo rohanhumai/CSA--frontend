22110deb0f6c6c343c418d7ef024cd23f83942d3
Rohan Coder <sablerohan125@gmail.com>
2026-02-20 17:37:47 +0530

    Changed Frenotend

---
+++ utils.api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_BASE = "/api";
+
+const api = axios.create({
+  baseURL: API_BASE,
+  headers: { "Content-Type": "application/json" },
+});
+
+// Attach token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Handle 401 globally
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/signin";
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
