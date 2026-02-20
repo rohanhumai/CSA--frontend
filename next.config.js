9ab583171cfa0ed920e1b7bf8513bd5179231f52
Rohan Coder <sablerohan125@gmail.com>
2026-02-20 17:29:41 +0530

    Changed Frenotend

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Required for Solana wallet adapter buffers
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      "stream": false,
+      "buffer": false
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig
